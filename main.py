bcbc812640617536e0226b1831c70e4b746dd8a9
Daniel Rodriguez <danielfrodriguez@unicesar.edu.co>
2025-03-04 10:12:17 -0500

    Implementar operaciones CRUD para usuarios en la API.

--- main.py
+++ main.py
@@ -6,14 +6,48 @@ from schema.user_schema import UserSchema
 app = FastAPI()
 conn = UserConnection()
 
-@app.get("/")
+@app.get("/") #Aca se muestra la lista de usuarios guardados en la BD
 def root():
-    conn 
-    return {"messege": "This is the root page of the API"}
+    items = []
+    for data in conn.read_all():
+        dictionary = {}
+        dictionary["id"] = data[0]
+        dictionary["name"] = data[1]
+        dictionary["phone"] = data[2]
+        items.append(dictionary)
+    return items
 
-@app.post("/users/insert")
+@app.get("/users/{id}") #Aca se muestra un usuario en especifico que esta almacenado en la BD
+def read_one(id:str):
+    data = conn.read_one(id)
+    if data is None:
+        raise HTTPException(status_code=404, detail="Data not found")
+    dictionary = {}
+    dictionary["id"] = data[0]
+    dictionary["name"] = data[1]
+    dictionary["phone"] = data[2]
+    return dictionary
+
+
+@app.post("/users/insert") #Aca hacemos un inset de un usuario a nuestra BD
 def insert(user_data: UserSchema):
     data = user_data.dict()
     data.pop("id")
     conn.write(data)
+    return {"messege": "Data inserted successfully"}
+
+
+@app.put("/users/update/{id}") #Aca actualizamos un usuario en especifico que esta almacenado en la BD
+def update(id: str, user_data: UserSchema):
+    data = user_data.dict()
+    data["id"] = id  # Añadir el id al diccionario de datos
+    conn.update(data)
+    return {"message": "Data updated successfully"}
+
+
+@app.delete("/users/delete/{id}")
+def delete(id:str):
+    conn.delete(id)
+    return {"messege": "Data deleted successfully"}
+
 
--- model/user_connection.py
+++ model/user_connection.py
@@ -1,6 +1,6 @@
 import psycopg
 
-class UserConnection():
+class UserConnection(): #Aca se hace la conexion a la BD, se le da la información de la BD. 
     conn = None
 
     def __init__(self):
@@ -19,14 +19,40 @@ class UserConnection():
             self.conn.close()
 
 
-    def write(self, data):
+    def write(self, data): #Aca se inserta un usuario a la BD
         with self.conn.cursor() as cur:
             cur.execute(""" 
             INSERT INTO users (name, phone) VALUES (%(name)s, %(phone)s) 
-""", data)
+            """, data)
             self.conn.commit()
-            
+
+    def read_all(self): #Aca se muestra la lista de usuarios guardados en la BD
+        with self.conn.cursor() as cur:
+            cur.execute("""
+            SELECT * FROM "users"
+                    """)
+            return cur.fetchall()
+        
+    def read_one(self, id): #Aca se muestra un usuario en especifico que esta almacenado en la BD
+        with self.conn.cursor() as cur:
+            cur.execute("""
+            SELECT * FROM "users" WHERE id = %s
+                    """, (id,))
+            return cur.fetchone()
+        
+    def update(self, data): #Aca actualizamos un usuario en especifico que esta almacenado en la BD
+        with self.conn.cursor() as cur:
+            cur.execute(""" UPDATE users SET name = %(name)s, phone = %(phone)s WHERE id = %(id)s
+                        """, data)
+            self.conn.commit()
+
+    def delete(self, id): #Aca se elimina un usuario en especifico que esta almacenado en la BD
+        with self.conn.cursor() as cur:
+            cur.execute("""
+            DELETE FROM "users" WHERE id = %s
+                        """, (id,))
+            self.conn.commit()
 
 
-    def __def__(self):
+    def __def__(self): #Aca se cierra la conexion a la BD
         self.conn.close()
